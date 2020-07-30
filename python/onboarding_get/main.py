b11f7b1fbb81081a5cc5d49c1848ae10858c6e9b
Kanyes Thaker <krthaker1224@berkeley.edu>
2020-07-29 22:37:46 -0700

    basic onboarding from DynamoDB

---
+++ python/onboarding_get/main.py
@@ -0,0 +1,49 @@
+import os, json, boto3
+from boto3.dynamodb.conditions import Key
+
+
+users = {'iris20':'17ef5c4b-3ac9-4548-a309-41e30a61c6e8', #Kanyes's Token/ID
+}
+def event_handler(event, context):
+	dynamodb = boto3.resource('dynamodb')
+	table = dynamodb.Table('iris_users')
+
+	if 'token' in event.keys():
+		token = event['token']
+	elif 'body' in event.keys():
+		token = json.loads(event['body'])['token']
+	else:
+		token = 'iris20'
+
+	try:
+		response = table.query(IndexName='token-index', KeyConditionExpression=Key('token').eq(token))
+		response = response["Items"][0]
+	except Exception as e:
+		return {"error":"Invalid token"}
+
+
+	name = response['name'].split(' ')[0]
+	preferences = response['preferences']
+	diet = preferences['diet']
+	cooking_level = preferences['cooking_level']
+	disliked_foods = ', '.join([e for e in preferences['disliked_foods']])
+	max_time_dinner = preferences['max_time_dinner']
+	max_time_lunch = preferences['max_time_lunch']
+	spice_tolerance = preferences['spice_tolerance']
+	top_cuisines = ', '.join([e for e in preferences['top_cuisines']])
+
+	ret = {}
+	ret['user_id'] = response['user_id']
+	ret['text'] = [
+		f"Hello, {name}",
+		f"{diet}",
+		f"{cooking_level} chef",
+		f"{max_time_lunch} for lunch",
+		f"{max_time_dinner} for dinner",
+		f"{spice_tolerance} spice",
+		f"Favorite cuisines are {top_cuisines}",
+		f"Dislikes {disliked_foods}",
+	]
+
+	return ret
+
