48830d222716cc83d93f37e364265547b28979ce
Kanyes Thaker <krthaker1224@berkeley.edu>
2020-08-04 05:11:33 -0700

    fixed endpoint for alpha launch

--- python/recipe_rate/main.py
+++ python/recipe_rate/main.py
@@ -1,4 +1,25 @@
 import boto3
 
 def event_handler(event, context):
-	return ":)"
+	dynamodb = boto3.resource('dynamodb')
+	table = dynamodb.Table('iris_users')
+
+	if 'user_id' in event.keys():
+		user_id = event['user_id']
+		recipe_id = event['recipe_id']
+	elif 'body' in event.keys():
+		user_id = json.loads(event['body'])['user_id']
+		recipe_id = json.loads(event['body'])['recipe_id']
+
+	field='disliked_recipes'
+	try:
+		table.update_item(
+			Key = {
+				'user_id': user_id
+			},
+			UpdateExpression="set disliked_recipes = list_append(disliked_recipes, :v)",
+			ExpressionAttributeValues={
+				':v': [recipe_id]
+			})
+	except Exception as e:
+		print(e)
